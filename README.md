6deeaa672bcdd3725739260c7ca0c5ea2bd13226
Tomasz Nocoń <tomasz.nocon@hotmail.com>
2017-05-25 20:45:46 +0200

    Change base image for Docker

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 The software for step motor control.
 
 docker run \
-    -e "MQTT_ADDRESS=mqtt://192.168.0.49" \
-    -e "PIN1=19" \ 
-    -e "PIN2=13" \
-    -e "PIN3=6" \
-    -e "PIN4=5" \
-    -e "DEVICE_ID=ENG01" \
-    -e "DEVICE_NAME=Engine controller" \
-    --privileged --device=/sys/class/gpio -d devicemanagment/device-engine
+-e "MQTT_ADDRESS=mqtt://192.168.0.49" \
+-e "PIN1=19" \
+-e "PIN2=13" \
+-e "PIN3=6" \
+-e "PIN4=5" \
+-e "DEVICE_ID=ENG01" \
+-e "DEVICE_NAME=Engine controller" \
+--privileged -d devicemanagment/device-engine
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { DeviceManager } from "./engine";
 
 let mqttAddress = process.env.MQTT_ADDRESS ? process.env.MQTT_ADDRESS : "mqtt://mqtt";
 let deviceId = process.env.DEVICE_ID ? process.env.DEVICE_ID : "ENG01";
+let deviceType = process.env.DEVICE_TYPE ? process.env.DEVICE_TYPE : "motor";
 let deviceName = process.env.DEVICE_NAME ? process.env.DEVICE_NAME : "Engine controller";
 let pin1Number = process.env.PIN1 ? parseInt(process.env.PIN1) : 19;
 let pin2Number = process.env.PIN2 ? parseInt(process.env.PIN2) : 13;
@@ -16,7 +17,7 @@ let engine = new DeviceManager.Engine(
         deviceId: deviceId,
         properties: {
             name: deviceName,
-            type: "motor",
+            type: deviceType,
             isOnline: true,
             rotation: 0
         }
@@ -33,6 +34,7 @@ let engine = new DeviceManager.Engine(
 
 console.log("MQTT address: " + mqttAddress);
 console.log("Device ID: " + deviceId);
+console.log("Device type: " + deviceType);
 console.log("Pin 1: " + pin1Number);
 console.log("Pin 2: " + pin2Number);
 console.log("Pin 3: " + pin3Number);
